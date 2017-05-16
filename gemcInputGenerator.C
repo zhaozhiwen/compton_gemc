02c7e441fbce738001bc82ae2e09502976d2d385
Xiaqing Li <xl79@login1.phy.duke.edu>
2017-05-16 15:45:47 -0400

    input file generator

---
+++ gemcInputGenerator.C
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "TROOT.h"
+#include "TLine.h"
+#include "TAxis.h"
+#include "TApplication.h"
+#include "Rtypes.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TString.h"
+#include "TRandom.h"
+#include "TCanvas.h"
+#include "TF1.h"
+#include "TH1F.h"
+#include "TH2D.h"
+#include "TMatrixD.h"
+#include "TEventList.h"
+#include "Math/Minimizer.h"
+#include "Math/Factory.h"
+#include "Math/Functor.h"
+#include "TError.h"
+#include "TMath.h"
+#include "TStyle.h"
+#include "TRandom2.h"
+
+using namespace std;
+
+
+void gemcInputGenerator(){
+
+  const nEvent = 100;
+  ofstream outfile("GemcInput.dat");
+
+  TFile *infile = new TFile("/home/xl79/mepg/git_Compton/brem_spectrum.root");
+  TH1F *hist = (TH1F*)infile->Get("brem_spectrum");
+  Double_t bincount;
+  Double_t E_min = 100.;
+  Double_t max = hist->GetBinContent(hist->FindBin(E_min));
+  Double_t val, prob, E;
+  Bool_t found;
+  TRandom2 rand;
+
+
+  for(int i=0; i<nEvent; i++){
+    outfile << "1\t0.\t0.\t0\t0\t0\t0.\t0.\t0.\t0."<< endl;
+    outfile << "1\t0.\t1\t22\t0\t0\t0.\t0.\t";
+
+    found = false;
+    while(!found){
+      val = E_min + rand.Rndm()*(1000.0-E_min);
+      prob = rand.Rndm()*max;
+      // val = Rx.Rndm()*7e7;
+      // prob = Ry.Rndm()*max;
+      bincount = hist->GetBinContent(hist->FindBin(val));
+
+      if(prob <= bincount){
+	E = val;
+	found = true;
+      }
+    }
+
+    outfile << E << "\t" << E << "\t0.\t0.\t0.\t100.0"<<endl;
+  }
+
+  outfile.close();
+
+}
