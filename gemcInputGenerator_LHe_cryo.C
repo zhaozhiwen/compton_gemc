9887fcbbc18b219f8aab58b38fd5a160903682dd
Xiaqing Li <xl79@grads-fa.internal.phy.duke.edu>
2017-11-08 15:52:18 -0500

    change input files

--- gemcInputGenerator_LHe_cryo.C
+++ gemcInputGenerator_LHe_cryo.C
@@ -28,7 +28,7 @@
 using namespace std;
 
 
-void gemcInputGenerator_LHe_cryo(int nEvent){
+void gemcInputGenerator_LHe_cryo(int nEvent, double E_min, double E_max){//E_min and E_max are in GeV
 
 //  const nEvent = 100;
   ofstream outfile("GemcInput.dat");
@@ -36,8 +36,9 @@ void gemcInputGenerator_LHe_cryo(int nEvent){
   TFile *infile = new TFile("brem_spectrum_Oct2017.root");
   TH1F *hist = (TH1F*)infile->Get("brem_spectrum");
   Double_t bincount;
-  Double_t E_min = 0.05; //GeV
-  Double_t max = hist->GetBinContent(hist->FindBin(E_min*1000));
+  // double E_min = 0.05; //GeV
+  // Double_t max = hist->GetBinContent(hist->FindBin(E_min*1000));
+  Double_t max = hist->GetMaximum();
   Double_t val, prob, E;
   Bool_t found;
   TRandom2 rand;
@@ -49,7 +50,7 @@ void gemcInputGenerator_LHe_cryo(int nEvent){
 
     found = false;
     while(!found){
-      val = E_min + rand.Rndm()*(1.0-E_min);
+      val = E_min + rand.Rndm()*(E_max-E_min);
       prob = rand.Rndm()*max;
       // val = Rx.Rndm()*7e7;
       // prob = Ry.Rndm()*max;
